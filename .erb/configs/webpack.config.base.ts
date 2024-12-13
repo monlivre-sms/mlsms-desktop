4aa1bf8a4ba805ac07d9a00557d10fd9f4aecf3f
Clément Costa <clement@offaxis.io>
2024-12-13 11:42:11 +0100

    Init

---
+++ .erb/configs/webpack.config.base.ts
@@ -0,0 +1,73 @@
+/**
+ * Base webpack config used across other specific configs
+ */
+
+import webpack from 'webpack';
+import TsconfigPathsPlugins from 'tsconfig-paths-webpack-plugin';
+import webpackPaths from './webpack.paths';
+import { dependencies as externals } from '../../release/app/package.json';
+
+const configuration: webpack.Configuration = {
+  externals: [...Object.keys(externals || {})],
+
+  stats: 'errors-only',
+
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            // Remove this line to enable type checking in webpack builds
+            transpileOnly: true,
+            compilerOptions: {
+              module: 'esnext',
+            },
+          },
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: webpackPaths.srcPath,
+    // https://github.com/webpack/webpack/issues/1114
+    library: {
+      type: 'commonjs2',
+    },
+  },
+
+  /**
+   * Determine the array of extensions that should be used to resolve modules.
+   */
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: [webpackPaths.srcPath, 'node_modules'],
+    // There is no need to add aliases here, the paths in tsconfig get mirrored
+    plugins: [new TsconfigPathsPlugins()],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+    //   assert: false,
+    //   net: false,
+    //   http: false,
+    //   https: false,
+      path: require.resolve("path-browserify"),
+      // fs: false,
+    //   tls: false,
+      stream: require.resolve("stream-browserify"),
+    //   zlib: false,
+      os: require.resolve("os-browserify/browser"),
+      vm: false,
+    }
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+    }),
+  ],
+};
+
+export default configuration;
